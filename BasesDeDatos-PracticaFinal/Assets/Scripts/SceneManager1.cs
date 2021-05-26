3a27425de12b7d509abd6be3d0ac93f147a19167
unknown <watafackah@gmail.com>
2021-05-27 00:02:07 +0200

    LinkBothSaveMethods
    Se han unido ambos metodos de guardado a través de un boton de cambio de escena

---
+++ BasesDeDatos-PracticaFinal/Assets/Scripts/SceneManager1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManager1 : MonoBehaviour
+{
+    public void Load(string scenename) 
+    {
+        Debug.Log("SceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+
+}
